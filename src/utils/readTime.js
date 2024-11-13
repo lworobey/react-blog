3300adee65d60f5a09d0344e952b30341f31153e
lworobey <worobeyluke@gmail.com>
2024-11-13 16:23:34 -0500

    Revert "feat: add read more toggle"

--- src/utils/readTime.js
+++
@@ -1,5 +0,0 @@
-export function calculateReadTime(content) {
-  const wordsPerMinute = 200;
-  const wordCount = content.trim().split(/\s+/).length;
-  return Math.ceil(wordCount / wordsPerMinute);
-}
